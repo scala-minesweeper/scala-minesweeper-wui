cdb416c4416622010d0c537249d1383ad495d05b
Simon Keller <simonkeller94@aol.com>
2018-01-01 23:02:12 +0100

    Show player history on web

--- app/controllers/WebSocketActor.scala
+++ app/controllers/WebSocketActor.scala
@@ -2,8 +2,9 @@ package controllers
 
 import akka.actor.{Actor, ActorRef, Props}
 import de.htwg.mps.minesweeper.controller._
-import de.htwg.mps.minesweeper.model.field.Field
 import de.htwg.mps.minesweeper.model.grid.Grid
+import de.htwg.mps.minesweeper.model.player.Player
+import de.htwg.mps.minesweeper.model.result.GameResult
 import play.api.libs.json._
 
 object WebSocketActor {
@@ -22,26 +23,36 @@ class WebSocketActor(webSocketOut: ActorRef, publisherActor: ActorRef, controlle
     case GridChanged(g) =>  webSocketOut ! gridEvent("GridChanged", g)
     case GameStart(g) => webSocketOut ! gridEvent("GameStart", g)
     case GameStatus(g) => webSocketOut ! gridEvent("GameStatus", g.grid())
+    case PlayerUpdate(p) => webSocketOut ! playerEvent("PlayerUpdate", p)
   }
 
   private def gridEvent(name: String, grid: Grid): JsValue =
     Json.toJson(WebSocketEvent(name, views.html.grid(grid).body))
 
-  private implicit val jsonWebSocketEventWriter: Writes[WebSocketEvent[String]] =
+  private def playerEvent(name: String, player: Player): JsValue =
+    Json.toJson(WebSocketEvent(name, player))
+
+  private implicit val jsonWebSocketEventString: Writes[WebSocketEvent[String]] =
     (webSocketEvent: WebSocketEvent[String]) => Json.obj(
       "name" -> webSocketEvent.name,
       "value" -> webSocketEvent.body
     )
 
-  private implicit val jsonGridWriter: Writes[Grid] =
-    (grid: Grid) => Json.obj(
-    "fields" -> grid.nestedFields
+  private implicit val jsonWebSocketEventPlayer: Writes[WebSocketEvent[Player]] =
+    (webSocketEvent: WebSocketEvent[Player]) => Json.obj(
+      "name" -> webSocketEvent.name,
+      "value" -> webSocketEvent.body
+    )
+
+  private implicit val jsonPlayer: Writes[Player] =
+    (player: Player) => Json.obj(
+    "history" -> player.history
   )
 
-  private implicit val jsonFieldWriter: Writes[Field] =
-    (field: Field) => Json.obj(
-    "value" -> field.toString(),
-    "state" -> field.fieldState.toString()
+  private implicit val jsonGameResult: Writes[GameResult] =
+    (gameResult: GameResult) => Json.obj(
+    "score" -> gameResult.getScore,
+    "win" -> gameResult.win
   )
 
   override def postStop(): Unit = {
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,14 +1,50 @@
-$(function() {
+$(function () {
     var socket = new WebSocket("ws://localhost:9000/socket");
-    socket.onopen = function(){
-        console.log('Connected to 127.0.0.1:9000!');
-    };
     socket.onmessage = function (event) {
-        renderGrid(JSON.parse(event.data).value)
+        handleEvent(JSON.parse(event.data));
     };
 
-    function renderGrid(gridHtml) {
-        $("#game-frame").html(gridHtml);
+    $("#restartGame").on("click", function () {
+        var cols = $("#inputCols").val();
+        var rows = $("#inputRows").val();
+        var bombs = $("#inputBombs").val();
+        if (cols > 0 && rows > 0) {
+            $.get("/newGame/" + rows + "/" + cols + "/" + bombs);
+        } else {
+            $.get("/newGame/10/10/10");
+        }
+    });
+
+    var eventHandlers = {
+        "FieldChanged": renderGrid,
+        "GridChanged": renderGrid,
+        "GameStart": renderGrid,
+        "GameStatus": renderGrid,
+        "PlayerUpdate": renderPlayer
+    };
+
+    function handleEvent(event) {
+        var handler = eventHandlers[event.name];
+        if (!handler) {
+            console.error("No handler configured for " + event.name);
+        } else {
+            handler(event)
+        }
+    }
+
+    function renderPlayer(event) {
+        var playerTable = $("#player-info").find("table").find("tbody");
+        playerTable.empty();
+        $.each(event.value.history, function (index, value) {
+            $('<tr>')
+                .append($("<td>").text(" " + (value.win ? "win" : "lost")))
+                .append($("<td>").text(value.score))
+                .appendTo(playerTable);
+        });
+    }
+
+    function renderGrid(event) {
+        $("#game-frame").html(event.value);
 
         var gridField = $(".grid-field");
 
@@ -26,17 +62,5 @@ $(function() {
             $.get("/toggleField/" + col + "/" + row);
             return false;
         });
-
-        $("#restartGame").on("click", function () {
-            var cols = $("#inputCols").val();
-            var rows = $("#inputRows").val();
-            var bombs = $("#inputBombs").val();
-            if (cols > 0 && rows > 0) {
-                $.get("/newGame/" + rows + "/" + cols + "/" + bombs);
-            } else {
-                $.get("/newGame/10/10/10");
-            }
-
-        })
     }
 });
